a246cc6e6c62c5e86c293af9ba82ca8c1654961e
Dominguez Leonardo <120284142+Odranoel998@users.noreply.github.com>
2023-10-19 13:26:47 -0300

    Creacion de tarjetas(MP-10)

--- mercado libre/src/components/Search.jsx
+++ mercado libre/src/components/Search.jsx
@@ -1,25 +1,25 @@
-import { Search } from "../views/HeadStyle"
-import { useNavigate, } from "react-router-dom";
+import { Search, ResultStyle, Tarjet, DescripcionStyle } from "../views/HeadStyle"
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 
 
 // eslint-disable-next-line react/prop-types
-export const SearchrHead = ({ value, handleChange, setEnter}) => {
+export const SearchrHead = ({ value, handleChange, setEnter }) => {
 
   const placeholder = '  Buscar productos,marcas y mas ...';
   const navigate = useNavigate()
   //--------------------------------------------------------------------------------
-  
-  
-  
+
+
+
   const handleKeyPress = (e) => {
     if (e.key === "Enter" && value != "") {
       e.preventDefault()
       navigate(`/search/${value}`);
       setEnter(true);
-      
+
     }
 
   };
@@ -40,16 +40,16 @@ export const SearchrHead = ({ value, handleChange, setEnter}) => {
 //Componente de result ---------------------------------------------------------------------------------------------------------
 
 // eslint-disable-next-line react/prop-types
-export const Result = ({ prompt, value, setID, setEnter, enter}) => {
+export const Result = ({ prompt, value, setID, setEnter, enter }) => {
   const [data, setData] = useState([]);
   const navigate = useNavigate()
 
 
 
-    
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${prompt}&limit=4`);
+        const response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${prompt}&limit=12`);
         const producto = response.data.results.map((result) => ({
           id: result.id,
           title: result.title,
@@ -68,48 +68,39 @@ export const Result = ({ prompt, value, setID, setEnter, enter}) => {
       }
     };
     //--------------------------------------------------------
-
-
-  if (prompt && enter==true ) {
-    fetchData();
-    setEnter(false)
-    
-
-
-  }
-
-
-
+    if (prompt && enter == true) {
+      fetchData();
+      setEnter(false)
+    }
+  }, [enter]);
   const selectDescription = (e) => {
     e.preventDefault()
     const respuesta = e.currentTarget.getAttribute('value');
     value = respuesta
     setID(value)
     navigate(`/search/:product/:${value}`)
-
   }
-
   //--------------------------------------------------------
   return (
-    <div>
+    <ResultStyle >
       <section >
         <article  >
           <ul>
             {data.map((item) => (
-              <div key={item.id} >
-                <img src={item.thumbnail} alt={item.title} />
+              <Tarjet key={item.id} value={item.id} onClick={selectDescription}>
+                <img src={item.thumbnail} alt={item.title} srcSet="" />
                 <div>
-                  <p>{item.id}</p>
-                  <p>${item.price}</p>
-                  <p value={item.id} onClick={selectDescription}>{item.title}</p>
+                  <hr></hr>
+                  <h3>${item.price}</h3>
+                  <p value={item.id}>{item.title}</p>
                 </div>
                 <span className="ml-auto">{item.seller_address.city.name}</span>
-              </div>
+              </Tarjet>
             ))}
           </ul>
         </article>
       </section>
-    </div>
+    </ResultStyle>
   );
 };
 
@@ -117,47 +108,57 @@ export const Result = ({ prompt, value, setID, setEnter, enter}) => {
 
 // eslint-disable-next-line react/prop-types
 export const ResultDescription = ({ prompt, value }) => {
-
   const [data, setData] = useState('');
+
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${value}&limit=4`);
+        const responseDescription = await axios.get(`https://api.mercadolibre.com/items/${prompt}/description`)
+        const fetchDescription = responseDescription.data.plain_text
         const productos = response.data.results;
         const producto = productos.find((result) => result.id === prompt);
-  
         if (producto) {
           setData({
             id: producto.id,
             title: producto.title,
             thumbnail: producto.thumbnail,
             price: producto.price,
+            descripcion: fetchDescription
           });
+
         } else {
-          alert("Producto no encontrado");
+          //alert("Producto no encontrado");
         }
-        console.log('ResultDescription: ', producto);
       } catch (error) {
         console.log(error);
       }
     };
     //--------------------------------------------------------
+
     if (prompt) {
       fetchData();
+      //console.log("llamado") 
     }
+
   }, [prompt]);
 
   return (
-    <div>
-      <p>El id del producto:{prompt}</p>
-      <img src={data.thumbnail} alt={data.title} />
-      <p>{data.price}</p>
+    <DescripcionStyle>
+      <div className="divTop">
+        <div className="divImg">
+          <img src={data.thumbnail} alt={data.title} />
+        </div>
+        <div>
+          <h2>{data.title}</h2>
+          <h3>${data.price}</h3>
+          <button>Comprar</button>
+        </div>
+      </div>
       <h2>Descripcion del producto</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Laboriosam pariatur harum magnam itaque consectetur id perferendis aperiam, amet et
-        doloribus obcaecati nulla alias atque ad tenetur, commodi laborum, dicta qui?</p>
-    </div>
+      <p>{data.descripcion}</p>
+    </DescripcionStyle>
   )
 }
 
---
+++ mercado libre/src/views/SearchStyle.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Tarjet = styled.div`
+    border: '2px solid red', 
+    width:'400px', 
+    display:'block'
+
+`
+
+export default {
+    Tarjet
+}
