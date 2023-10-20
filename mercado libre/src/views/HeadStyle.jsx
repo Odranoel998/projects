8dba358f91d3f958e7dd82e7565da981f7e85ec3
Dominguez Leonardo <120284142+Odranoel998@users.noreply.github.com>
2023-10-20 17:51:39 -0300

    Recreacion del componente ResultDescription

--- mercado libre/src/views/HeadStyle.jsx
+++ mercado libre/src/views/HeadStyle.jsx
@@ -16,69 +16,69 @@ export const Head = styled.div`
 
 export const Search = styled.input`
 border-color: transparent;
-border-radius:5px
-margin-top: -5px;
-margin-left: 16.5%;
-display: block;
-width: 600px;
-height: 40px;
+border-radius:5px;
+margin-top:-5px;
+margin-left:16.5%;
+display:block;
+width:600px;
+height:40px;
 
 `
-export const DescripcionStyle= styled.div`
-background-color: white;
-margin-left: 10%; 
-margin-top: 3%;
-border-radius: 8px;
-width: 800px;
-min-height: 500px; 
-display: inline-block;
-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-overflow: hidden; 
-    .divTop{
-        display: flex;
-        //border: 2px solid blue;
-        width: 780px;
-        hedth: 50px;
+export const DescripcionStyle = styled.div`
+// background-color: white;
+// margin-left: 10%; 
+// margin-top: 3%;
+// border-radius: 8px;
+// width: 800px;
+// min-height: 500px; 
+// display: inline-block;
+// box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+// overflow: hidden; 
+//     .divTop{
+//         display: flex;
+//         //border: 2px solid blue;
+//         width: 780px;
+//         hedth: 50px;
 
-        .divImg{ 
-            display:flex;
-            justify-content: flex-start;
-            //border: 2px solid red;
-            width: 480px;
-            img{
-                margin-left:50px;
-                width: 50%;
-                hedth: 50%;  
-            }
-        }
-        div{
-            margin-top: 20px;
-            maring-left: 20px;
-            //border: 2px solid yellow;
-            width: 250px;
-            hedth: 50px;
+//         .divImg{ 
+//             display:flex;
+//             justify-content: flex-start;
+//             //border: 2px solid red;
+//             width: 480px;
+//             img{
+//                 margin-left:50px;
+//                 width: 50%;
+//                 hedth: 50%;  
+//             }
+//         }
+//         div{
+//             margin-top: 20px;
+//             maring-left: 20px;
+//             //border: 2px solid yellow;
+//             width: 250px;
+//             hedth: 50px;
 
-            h3{
-                font-size:30px
-            }
-            button{
-                color:white;
-                background-color:#165cc8;
-                border: transparent;
-                width: 150px;
-                padding: 10px;
-                border-radius:8px
-            }
-        }
-    }
-    h2{
-        margin:5px;
-    }
-    p{
-        //border: 2px solid black;
-        font-size: 15px;
-        margin:5px;
-    }
+//             h3{
+//                 font-size:30px
+//             }
+//             button{
+//                 color:white;
+//                 background-color:#165cc8;
+//                 border: transparent;
+//                 width: 150px;
+//                 padding: 10px;
+//                 border-radius:8px
+//             }
+//         }
+//     }
+//     h2{
+//         margin:5px;
+//     }
+//     p{
+//         //border: 2px solid black;
+//         font-size: 15px;
+//         margin:5px;
+//     }
 `
 
 export const ResultStyle = styled.div`
@@ -139,6 +139,141 @@ overflow: hidden;
         }
     }
 `;
+export const SelectDescription = styled.div`
+border: 2px solid violet;
+background-color:white;
+margin: 20px;
+display: flex;
+flex-direction: column;
+    
+    .divThree{
+        //border: 2px solid red;
+        background-color: white;
+        font-family: Helvetica, Arial, sans-serif;
+        margin: 20px;
+        width:1300px;
+        min-height:100%;
+        display: flex;
+        flex-direction: row;
+        justify-content:flex-end;
+
+        .divImg{
+            //border: 2px solid blue;
+            margin-top: 10px;
+            background-color: white;
+            width:500px;
+            height:100%;
+            display: flex;
+            flex-direction: row;
+            justify-content:flex-end;
+            img{
+                //border: 2px solid blue;
+                width:70%;
+                height: 100%;
+                margin-right: 10px;
+            }
+        }
+        .divInfo{
+            //border: 2px solid green;
+            margin-top: 10px;
+            background-color: white;
+            width:400px;
+            height:100%;
+        }
+        .divTarget{
+            //border: 2px solid yellow;
+            margin-top: 10px;
+            background-color: white;
+            width:400px;
+            height:100%;
+            display: flex;
+            flex-direction: column;
+            
+
+            .divButton{
+                border: 1px solid grey;
+                margin: 15px;
+                margin-left:3%;
+                border-radius: 8px;
+                min-height:500px;
+                display: flex;
+                flex-direction: column;
+
+                .pText{
+                    //border: 1px solid black;
+                    font-size: 14px;
+                    margin-left: 5%;
+                    .pBlue1{
+                    //border: 1px solid #3483FA;
+                    font-size: 12px;
+                    color:#3483FA;
+                    margin-top: 1%;
+                    margin-left: 0%;
+                    display: flex;
+                    flex-direction: column;
+                    }
+                }
+                .pTextBlack{
+                    //border: 1px solid black;
+                    font-size: 14px;
+                    font-weight: bold;
+                    margin-left: 5%;
+                    .pTextBlackSub{
+                        margin-top: 0px;
+                        font-size: 12px;
+                        font-weight: lighter;
+                        color: grey;
+                    }
+                    
+                }
+                .pBlue{
+                    //border: 1px solid #3483FA;
+                    font-size: 12px;
+                    color:#3483FA;
+                    margin-left: 10%;
+                    display: flex;
+                    flex-direction: column;
+                
+                    .pTextBlue{
+                        //border: 1px solid black;
+                        margin-top: 1px;
+                        margin-left: 1px;
+                        color:black;
+                    }
+                }
+                .buttonShopping{
+                    color:white;
+                    background-color:#2968C8;
+                    border: transparent;
+                    width: 250px;
+                    padding: 17px;
+                    border-radius:8px;
+                    margin:5px;
+                    margin-left: 15%;
+                }
+                .buttonAdd{
+                    color:#3483FA;
+                    background-color:#D9E7FA;
+                    border: transparent;
+                    width: 250px;
+                    padding: 17px;
+                    border-radius:8px;
+                    margin:5px;
+                    margin-left: 15%;
+                }
+            }
+            .divInfoStore{
+                border: 1px solid grey;
+                margin: 15px;
+                margin-left:3%;
+                border-radius: 8px;
+                min-height:500px;
+                display: flex;
+                flex-direction: column;
+            }
+        }
+    }
+`;
 
 export const HeadCenter = styled.div`
 display: inline;
@@ -166,7 +301,7 @@ export default {
     Search,
     ResultStyle,
     Tarjet,
-    DescripcionStyle,
+    SelectDescription,
     HeadCenter,
     HeadRight,
     ButtonStyle,
